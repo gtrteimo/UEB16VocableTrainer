0f831cef906eafa88eb6739f5b4f2c1ab7b7e39f
gtrteimo <133528796+gtrteimo@users.noreply.github.com>
2024-11-26 20:47:48 +0100

    New Start
    Blank Slate, removed everything to start anew

--- src/net/tfobz/vocabletrainer/gui/VocableTrainerPanelInterface.java
+++
@@ -1,9 +0,0 @@
-package net.tfobz.vocabletrainer.gui;
-
-/**
- * Interface containing all essential methods used by the panels
- * @author gtrteimo
- */
-public interface VocableTrainerPanelInterface {
-	public void updateSize(int width, int heigth);
-}
