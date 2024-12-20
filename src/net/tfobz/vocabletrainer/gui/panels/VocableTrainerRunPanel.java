ca2ebefa66d73041cad976c69029e02a3caae301
gtrteimo <133528796+gtrteimo@users.noreply.github.com>
2024-12-20 13:01:55 +0100

    -
    -

--- src/net/tfobz/vocabletrainer/gui/panels/VocableTrainerRunPanel.java
+++ src/net/tfobz/vocabletrainer/gui/panels/VocableTrainerRunPanel.java
@@ -14,45 +14,59 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class VocableTrainerRunPanel extends VocableTrainerPanel {
 	
+	private VocableTrainerRunSettings settings;
 	
-	//private VocableTrainerRunSettings settings;
+	
 	private int[] times;
 	private int result;
 	private int cardNum;
 	private ArrayList<Karte> cards;
 	private Karte currentCard;
+	
+	
 	private ClockLabel clock1;
 	private ClockLabel clock2;
 	
+	private JLabel originalWord;
+	private JLabel answer;
+
+	private JTextField input;
+	
+	private JButton stop;
+	private JButton skip;
+	private JButton next;
+
+	
 	public VocableTrainerRunPanel (VocableTrainerFrame vtf, VocableTrainerRunSettings settings) {
-		super(vtf);
-		//this.settings = settings;
+		super();
+		this.vtf = vtf;
+		this.settings = settings;
 		
-		setBackground(new Color(225, 225, 225, 255));
-		
+		setBackground(C_platinum);
 		setLayout(null);
+		
+		panel = new VocableTrainerPanel(this);
+		panel.setLocation(16, 16);
+		panel.setBackground(C_powderBlue);
 
         times = settings.isCardLimit()?new int[settings.getCardLimit()]: new int[VokabeltrainerDB.getKarten(settings.getBox().getNummer()).size()];
         cardNum = 0;
         
         cards = VokabeltrainerDB.getKarten(settings.getBox().getNummer());
         
-        
-
 		clock1 = new ClockLabel("Total Time: ", settings.isTotalTimeLimit()?settings.getTotalTimeLimit():0, ()->endRun());
-        clock1.setBounds(10, 10, 200, 30);
-        clock1.setFont(new Font("Arial", Font.PLAIN, 30));
-        this.add(clock1);
         clock2 = new ClockLabel("Card Time: ", settings.isCardTimeLimit()?settings.getCardTimeLimit():0,()->nextCard());
-        clock2.setBounds(10, 40, 200, 30);
-        clock2.setFont(new Font("Arial", Font.PLAIN, 30));
-        this.add(clock2);
         
         clock1.start();
         clock2.start();
         
 		currentCard = cards.get((int)(Math.random()*cards.size()));
         cards.remove(currentCard);
+        
+        panel.add(clock1);
+        panel.add(clock2);
+        
+        add(panel);
 	}
 	
 	public void endRun() {
@@ -72,6 +86,19 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
 	public void checkCard() {
 		times[cardNum] = clock2.getTime();
 		//TODO
+	}
+	
+	@Override
+	public void paintComponent (Graphics g) {
+		super.paintComponent(g);
+		
+		panel.setSize(getWidth()-32, getHeight()-32);
+		
+		clock1.setBounds(10, 10, 200, 30);
+        clock1.setFont(new Font("Arial", Font.PLAIN, clock1.getHeight()/2));
+        
+        clock2.setBounds(10, 40, 200, 30);
+        clock2.setFont(new Font("Arial", Font.PLAIN, clock2.getHeight()/2));
 	}
 	
 	private class ClockLabel extends JLabel {
