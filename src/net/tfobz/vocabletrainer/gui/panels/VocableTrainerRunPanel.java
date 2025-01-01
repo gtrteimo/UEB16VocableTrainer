edd24b22b1c0719369141e7e99a9af46b84028ab
21chinat <nathan.chizzali06@gmail.com>
2025-01-01 17:12:13 +0100

    It now work a bit more than before

--- src/net/tfobz/vocabletrainer/gui/panels/VocableTrainerRunPanel.java
+++ src/net/tfobz/vocabletrainer/gui/panels/VocableTrainerRunPanel.java
@@ -58,6 +58,7 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
 		
         cards = VokabeltrainerDB.getKarten(settings.getBox().getNummer());
         times = settings.isCardLimit()?new int[settings.getCardLimit()]: new int[cards.size()];
+        results = new int[times.length];
         cardNum = -1;
 
         clock1 = new JLabel("Total Time: 0");
@@ -65,9 +66,47 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
         originalWord = new JLabel();
         answer = new JLabel();
         input = new JTextField();
-        stop = new JButton();
-        skip = new JButton();
-        next = new JButton();
+        stop = new JButton("Stop");
+        skip = new JButton("Skip");
+        next = new JButton("Check");
+        
+        originalWord.setVerticalAlignment(SwingConstants.TOP);
+        originalWord.setHorizontalAlignment(SwingConstants.CENTER);
+        answer.setVerticalAlignment(SwingConstants.TOP);
+        answer.setHorizontalAlignment(SwingConstants.CENTER);
+        input.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        stop.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        skip.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				input.setText("");
+				checkCard();
+				nextCard();
+			}
+		});
+        
+        next.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(timer2.isRunning()) {
+					checkCard();
+				} else {
+					nextCard();
+				}
+				
+			}
+		});
         
         timer1 = new Timer(1000, new ActionListener() {
 			@Override
@@ -87,7 +126,6 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
 				clock2.setText("Card Time: "+time2);
 				if(settings.isCardTimeLimit()&&time2>=settings.getCardTimeLimit()) {
 					checkCard();
-					nextCard();
 				}
 			}
 		});
@@ -121,6 +159,13 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
 			currentCard = cards.get((int)(Math.random()*cards.size()));
 	        cards.remove(currentCard);
 	        originalWord.setText(currentCard.getWortEins());
+	        skip.setVisible(true);
+			next.setText("Check");
+			input.setBackground(Color.WHITE);//TODO correct Color
+			input.setText("");
+			answer.setText("");
+	        time2=0;
+	        timer2.start();
 		}
 	}
 	
@@ -136,8 +181,10 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
 			}
 			results[cardNum]= correct?1:-1;
 			input.setBackground(correct?Color.GREEN:Color.RED);
-		    answer.setText("<html>Correct answer <span style='color:green;'>"+currentCard.getWortZwei()+"</span></html>");
 		}
+	    answer.setText("<html>Correct answer: <span style='color:green;'>"+currentCard.getWortZwei()+"</span></html>");
+		skip.setVisible(false);
+		next.setText("Next");
 	}
 	
 	@Override
@@ -145,28 +192,31 @@ public class VocableTrainerRunPanel extends VocableTrainerPanel {
 		
 		super.paintComponentRun(g);
 		
-		clock1.setBounds(10, 10, 200, 30);
+		int width = panel.getWidth();
+		int height = panel.getHeight();
+		
+		clock1.setBounds(10, 10, width-20, height/9-10);
         clock1.setFont(new Font("Arial", Font.PLAIN, clock1.getHeight()/2));
         
-        clock2.setBounds(10, 40, 200, 30);
+        clock2.setBounds(10, 10+height/9, width-20, height/9-10);
         clock2.setFont(new Font("Arial", Font.PLAIN, clock2.getHeight()/2));
         
-        originalWord.setBounds(10, 70, panel.getWidth(), 20);
-        originalWord.setFont(new Font("Arial", Font.PLAIN, originalWord.getHeight()/2));
+        originalWord.setBounds(10, 10+height/9*2, width-20, height/9*2-10);
+        originalWord.setFont(new Font("Arial", Font.PLAIN, originalWord.getHeight()/4));
         
-        answer.setBounds(10, 100, panel.getWidth(), 20);
+        answer.setBounds(10, 10+height/9*4, width-20, height/9-10);
         answer.setFont(new Font("Arial", Font.PLAIN, answer.getHeight()/2));
         
-        input.setBounds(10, 130, panel.getWidth(), 20);
-        input.setFont(new Font("Arial", Font.PLAIN, input.getHeight()/2));
+        input.setBounds(10, 10+height/9*5, width-20, height/9*2-10);
+        input.setFont(new Font("Arial", Font.PLAIN, input.getHeight()/4));
         
-        stop.setBounds(10, 160, 100, 30);
+        stop.setBounds(10, 10+height/9*7, (width-40)/3, height/9*2-20);
         stop.setFont(new Font("Arial", Font.PLAIN, stop.getHeight()/2));
         
-        skip.setBounds(110, 160, 100, 30);
+        skip.setBounds(20+(width-40)/3, 10+height/9*7, (width-40)/3, height/9*2-20);
         skip.setFont(new Font("Arial", Font.PLAIN, skip.getHeight()/2));
         
-        next.setBounds(210, 160, 100, 30);
+        next.setBounds(30+(width-40)/3*2, 10+height/9*7, (width-40)/3, height/9*2-20);
         next.setFont(new Font("Arial", Font.PLAIN, next.getHeight()/2));
     }
 }
